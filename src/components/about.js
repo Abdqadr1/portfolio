497724166d9f0f1596ac1c7a1cda7e866c67641f
Abdqadr1 <abolarinwaquadri@gmail.com>
2022-09-13 16:52:11 +0800

    about sect

--- src/components/about.js
+++ src/components/about.js
@@ -2,29 +2,43 @@ import { useEffect, useRef } from "react";
 import profile from "../images/profile.png";
 import observe from "./observer";
 const About = () => {
-    const aboutRef = useRef();
-    useEffect(() => {
-        if (aboutRef.current) {
-            observe(aboutRef.current);
-        }
-    })
-    return ( 
-        <>
-             <section ref={aboutRef} id="About" className="about text-start" data-animate="slide">
-                <div className="about-container">
-                    <img src={profile} alt="profile" className="left-in" />
-                    <div className="about-content right-in" tabIndex="0">
-                        <h2 className="heading-secondary">About me</h2>
-                        <p className="text-paragraph-16">I'm Ronny. I love connecting with people and learning. I am a fulltime web developer</p>
-                        <p className="text-paragraph-16">I'm a full stack developer focusing on the creation and deployment of usable and enjoyable
-                            web applications. I'm also an entry level DevOps engineer and I would like an opportunity get better in that regard.</p>
-                        <p className="text-paragraph-16">Contact me for contract/freelance work, consulting, or if you want to chat! I'd be
-                        happy to hear from you.</p>
-                    </div>
-                </div>
-            </section>
-        </>
-     );
-}
- 
+  const aboutRef = useRef();
+  useEffect(() => {
+    if (aboutRef.current) {
+      observe(aboutRef.current);
+    }
+  });
+  return (
+    <>
+      <section
+        ref={aboutRef}
+        id="About"
+        className="about text-start"
+        data-animate="slide"
+      >
+        <div className="about-container">
+          <img src={profile} alt="profile" className="left-in" />
+          <div className="about-content right-in" tabIndex="0">
+            <h2 className="heading-secondary">About me</h2>
+            <p className="text-paragraph-16">
+              I'm Quadri. I love connecting with people and learning. I am a
+              fulltime web developer
+            </p>
+            <p className="text-paragraph-16">
+              I'm a full stack developer focusing on the creation and deployment
+              of usable and enjoyable web applications. I'm also an entry level
+              DevOps engineer and I would like an opportunity get better in that
+              regard.
+            </p>
+            <p className="text-paragraph-16">
+              Contact me for contract/freelance work, consulting, or if you want
+              to chat! I'd be happy to hear from you.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default About;
